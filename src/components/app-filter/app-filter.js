fd6d2f126e34f35e809813cd1bba23c0e864877d
Regina <reina5425@gmail.com>
2022-10-04 20:51:04 +0300

    добавлен поиск и фильтр

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,17 +1,28 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+	const buttonsData = [
+		{name: 'all', label: 'Все сотрудники'},
+		{name: 'rise', label: 'На повышение'},
+		{name: 'moreThan1000', label: 'З/П больше 1000$'}
+	];
+
+	const buttons = buttonsData.map(({name, label}) => {
+		const active = props.filter === name;
+		const clazz = active ? 'btn-light' : 'btn-outline-light';
+		return (
+			<button className={`btn ${clazz}`} 
+				type="button" 
+				key={name}
+				onClick={() => props.onFilterSelect(name)}>
+				{label}
+			</button>
+		)
+	})
+
 	return (
 		<div className="btn-group">
-			<button className="btn btn-light" type="button">
-				Все сотрудники
-			</button>
-			<button className="btn btn-outline-light" type="button">
-				На повышение
-			</button>
-			<button className="btn btn-outline-light" type="button">
-				З/П больше 1000$
-			</button>
+			{buttons}
 		</div>
 	);
 }
